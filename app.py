33a3a6b70aeef714f2cabd4d4ae572dadd8ccff7
Rajat Gupta <rajat19g@gmail.com>
2024-05-01 13:17:39 +0000

    Update file after merge

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def handle_webhook():
     payload = json.loads(payload['payload'])
     #print(payload['ref'])
     #print(type(payload.get('ref')))
-    if (headers.get('X-GitHub-Event') == 'push') & (payload['ref'] == 'refs/heads/main'):
+    if (headers.get('X-GitHub-Event') == 'push') & (payload['ref'] == 'refs/heads/main') & (payload['head_commit']['committer'] == 'noreply@github.com'):
         # Extract information about the merge event
         #base_branch = payload['base']['ref']
         #merged_branch = payload['ref']
@@ -29,11 +29,37 @@ def handle_webhook():
     else:
         return 'No actions taken', 200
 
-def update_file(payload):
+@app.route('/webhook-pull', methods=['POST'])
+def handle_webhook_pull():
+    
+    headers = request.headers
+    if (headers.get('X-GitHub-Event') != 'pull_request'):
+        return 'No actions taken', 200
+    
+
+    
+    payload = request.form 
+    payload = json.loads(payload['payload'])
+    #print(payload['ref'])
+    #print(type(payload.get('ref')))
+    if  payload['action'] == 'closed' :
+        # Extract information about the merge event
+        #base_branch = payload['base']['ref']
+        #merged_branch = payload['ref']
+        #print(base_branch,merged_branch)
+        # Trigger file update process
+        update_file()
+    
+        return 'Webhook received', 200
+    
+    else:
+        return 'No actions taken', 200
+
+def update_file():
     # Fetch the latest version of the file from the repository
     subprocess.run(['git', 'pull'])
-    with open('test.txt','a') as file:
-        file.write('Commit made by bot what   dffd d  ssssss')
+    with open('test.txt','w') as file:
+        file.write('Final Commit made by bot')
     # Perform necessary updates to the file
     # Example: Replace placeholder values in the file with new values
 
